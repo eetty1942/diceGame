2fb924c2ecd0b748d18d3652ce0a3bd2aada6ac9
eetty1942 <eetty1942@gmail.com>
2015-01-15 00:05:05 +0900

    전체 기능 작업

--- src/com/maple/Player.java
+++ src/com/maple/Player.java
@@ -7,7 +7,7 @@ public class Player {
 	public Scanner scanner = new Scanner(System.in);
 	
 	public selectMode(){ 
+		
+	}
 
-	System.out.println("번플레이어의 이름을 쓰시오 : ");
-	playerName = scanner.next();
 }
